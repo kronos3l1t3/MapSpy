6e996df2ab10e30e08ac636d84ca84c5fbf2e685
kronos3l1t3 <kronos3l1t3@gmail.com>
2022-03-17 02:49:41 -0400

    port

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const csp = require('content-security-policy');
 const app = express();
 const path = require("path");
 const publicPath = path.join(__dirname, "..", "build");
 const port = process.env.PORT || 3001;
 const cors = require("cors")
 
+
+const cspPolicy = {
+    'report-uri': '/reporting',
+    'default-src': csp.SRC_NONE,
+    'script-src': [ csp.SRC_SELF, csp.SRC_DATA ]
+};
+
+const globalCSP = csp.getCSP(csp.STARTER_OPTIONS);
+const localCSP = csp.getCSP(cspPolicy);
+
+app.use(globalCSP);
 
 app.use(express.static(publicPath));
 app.use(cors());
