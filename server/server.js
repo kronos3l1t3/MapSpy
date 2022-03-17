967961246742e2697420cc9278901f9802a7cb8d
kronos3l1t3 <kronos3l1t3@gmail.com>
2022-03-17 03:11:32 -0400

    json

--- server/server.js
+++ server/server.js
@@ -5,7 +5,19 @@ const path = require("path");
 const publicPath = path.join(__dirname, "..", "build");
 const port = process.env.PORT || 3001;
 const cors = require("cors")
+const helmet = require("helmet");
 
+
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+                "script-src": ["'self'", "'unsafe-inline'"],
+            },
+        },
+    })
+);
 
 const cspPolicy = {
     'report-uri': '/reporting',
